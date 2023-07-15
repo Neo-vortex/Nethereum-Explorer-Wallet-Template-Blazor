e5793db27c435212a433ec164a52b0892f065920
Mohamadreza Nakhleh <neo.vortex@pm.me>
2023-07-15 08:04:37 +0330

    (fix) remove infura

--- NethereumExplorer.Core/Services/Web3ProviderService.cs
+++ NethereumExplorer.Core/Services/Web3ProviderService.cs
@@ -6,7 +6,7 @@ namespace NethereumExplorer.Services
 {
     public class Web3ProviderService: IWeb3ProviderService
     {
-        public string CurrentUrl { get; set; } = "https://mainnet.infura.io/v3/ddd5ed15e8d443e295b696c0d07c8b02";
+        public string CurrentUrl { get; set; } = "https://chainnova-scf-chain.darkube.app";
 
         //TODO: Simple chainId workaround, this should be the ChainId from the connection, when adding the url we should get the chainId using rpc and add it here.
         public string ChainId => CurrentUrl;
